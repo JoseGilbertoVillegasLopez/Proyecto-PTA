b9a2d8db0399e053300ffafbb09a2bf8d82f6ce9
Jose Gilberto VIllegas Lopez <josegibertov2@gmail.com>
2025-10-04 16:13:35 -0600

    crea crud de la entidad usuario

---
+++ src/Form/UsuarioType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Personal;
+use App\Entity\Usuario;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UsuarioType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('usuario')
+            ->add('password')
+            ->add('rol')
+            ->add('activo')
+            ->add('personal', EntityType::class, [
+                'class' => Personal::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Usuario::class,
+        ]);
+    }
+}
